6aaf5679550cfd2d9ac490c3f9589e808a0aeec0
U2Ethereal <844073818@qq.com>
2021-08-12 02:29:59 +0800

    fix location

--- src/views/Table3.vue
+++ src/views/Table3.vue
@@ -1,5 +1,7 @@
 <template>
     <div id="container"></div>
+    <div class="info">
+</div>
 </template>
 
 
@@ -11,39 +13,42 @@ export default{
     AMapLoader.load({
     "key": "88c17c713cd24a59ad64b7e256449d1a",              // 申请好的Web端开发者Key，首次调用 load 时必填
     "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-    "plugins": ['AMap.Scale','AMap.ToolBar','AMap.CitySearch','AMap.MapType'],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+    "plugins": ['AMap.Scale','AMap.ToolBar','AMap.Geolocation','AMap.MapType'],           // 需要使用的的插件列表，如比例尺'AMap.Scale'等
     }).then((AMap)=>{
-    let map = new AMap.Map('container');
+    let map = new AMap.Map('container',{
+        resizeEnable: true
+    });
+    let geolocation = new AMap.Geolocation({
+    enableHighAccuracy: true,//是否使用高精度定位，默认:true
+        timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+        maximumAge: 0,           //定位结果缓存0毫秒，默认：0
+        convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+        showButton: false,        //显示定位按钮，默认：true
+        showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
+        showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
+        panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
+        zoomToAccuracy:true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+    });
+    map.addControl(geolocation);
     map.addControl(new AMap.Scale());
     map.addControl(new AMap.ToolBar());
     map.addControl(new AMap.MapType());
     map.add(new AMap.Marker({
             position:map.getCenter()
         }));
-    //实例化城市查询类
-    const citysearch = new AMap.CitySearch();
-    //自动获取用户IP，返回当前城市
-    citysearch.getLocalCity(function(status, result) {
-        if (status === 'complete' && result.info === 'OK') {
-            if (result && result.city && result.bounds) {
-                let citybounds = result.bounds;
-                map.setBounds(citybounds);
-            }
-        }
-    });
+    geolocation.getCurrentPosition();
     }).catch(e => {
         console.log(e);
     });
 },
-
 }
 </script>
 
 <style scoped>
     body {
-            margin: 0;
-            padding: 0
-        }
+        margin: 0;
+        padding: 0
+    }
 
     html,
     body,
